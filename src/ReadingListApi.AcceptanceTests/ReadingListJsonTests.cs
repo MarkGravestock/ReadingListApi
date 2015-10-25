89afdd390dc33cc75f58a493c39f02decadb8000
Mark Gravestock <mark.gravestock@gmail.com>
2015-10-25 22:43:51 +0000

    Refactor Test.

--- src/ReadingListApi.AcceptanceTests/ReadingListJsonTests.cs
+++ src/ReadingListApi.AcceptanceTests/ReadingListJsonTests.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using Microsoft.AspNet.Hosting;
-using Microsoft.Dnx.Runtime.Infrastructure;
-using Microsoft.Framework.Configuration;
 using Xunit;
 
 namespace ReadingListApi.AcceptanceTests
@@ -14,18 +10,12 @@ namespace ReadingListApi.AcceptanceTests
         public void ReturnsExpectedHttpResult()
         {
             var uri = "http://localhost:5123";
-            var baseAddress = new Uri(uri);
-            var configBuider = new ConfigurationBuilder();
 
-            configBuider.AddInMemoryCollection(new Dictionary<string, string> {{"server.urls", uri}});
-            var builder = new WebHostBuilder(CallContextServiceLocator.Locator.ServiceProvider, configBuider.Build());
-            builder.UseStartup<Startup>();
-            builder.UseServer("Microsoft.AspNet.Server.Kestrel");
+            var host = new TestWebHostBuilder().UsingUri(uri).Build();
 
-            var host = builder.Build();
             using (var app = host.Start())
             {
-                var client = new HttpClient {BaseAddress = baseAddress};
+                var client = new HttpClient {BaseAddress = new Uri(uri)};
 
                 var response = client.GetAsync("api/ReadingList").Result;
 
---
+++ src/ReadingListApi.AcceptanceTests/TestWebHostBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Hosting.Internal;
+using Microsoft.Dnx.Runtime.Infrastructure;
+using Microsoft.Framework.Configuration;
+
+namespace ReadingListApi.AcceptanceTests
+{
+    public class TestWebHostBuilder
+    {
+        private string uri;
+
+        public TestWebHostBuilder UsingUri(string uri)
+        {
+            this.uri = uri;
+            return this;
+        }
+
+        public IHostingEngine Build()
+        {
+            var baseAddress = new Uri(uri);
+            var configBuider = new ConfigurationBuilder();
+
+            configBuider.AddInMemoryCollection(new Dictionary<string, string> {{"server.urls", uri}});
+            var builder = new WebHostBuilder(CallContextServiceLocator.Locator.ServiceProvider, configBuider.Build());
+            builder.UseStartup<Startup>();
+            builder.UseServer("Microsoft.AspNet.Server.Kestrel");
+
+            return builder.Build();
+        }
+    }
+}
