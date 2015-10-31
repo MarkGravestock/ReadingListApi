82e93a081bfdf9cdfe598e4532dcf60d274d46e6
Mark Gravestock <mark.gravestock@gmail.com>
2015-10-31 16:26:36 +0000

    Move acceptance test into test folder.

--- src/ReadingListApi.AcceptanceTests/ReadingListJsonTests.cs
+++ src/ReadingListApi.AcceptanceTests/ReadingListJsonTests.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Hosting.Internal;
+using System;
 using System.Net.Http;
 using Xunit;
 
@@ -6,20 +7,38 @@ namespace ReadingListApi.AcceptanceTests
 {
     public class ReadingListJsonTests
     {
+        public readonly string HostUri = "http://localhost:5123";
+        public readonly string ReadingListPath = "api/ReadingList";
+        public readonly string MediaTypeJson = "application/json";
+
+        public readonly IHostingEngine host;
+        public readonly HttpClient client;
+
+        public ReadingListJsonTests()
+        {
+            host = new TestWebHostBuilder().UsingUri(HostUri).Build();
+            client = new HttpClient { BaseAddress = new Uri(HostUri) };
+        }
+
         [Fact]
         public void ReturnsExpectedHttpResult()
         {
-            var uri = "http://localhost:5123";
-
-            var host = new TestWebHostBuilder().UsingUri(uri).Build();
-
             using (var app = host.Start())
             {
-                var client = new HttpClient {BaseAddress = new Uri(uri)};
-
-                var response = client.GetAsync("api/ReadingList").Result;
+                var response = client.GetAsync(ReadingListPath).Result;
 
                 Assert.True(response.IsSuccessStatusCode, "Actual status code: " + response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public void ReturnsExpectedJsonContent()
+        {
+            using (var app = host.Start())
+            {
+                var response = client.GetAsync(ReadingListPath).Result;
+
+                Assert.Equal(MediaTypeJson, response.Content.Headers.ContentType.MediaType);
             }
         }
     }
