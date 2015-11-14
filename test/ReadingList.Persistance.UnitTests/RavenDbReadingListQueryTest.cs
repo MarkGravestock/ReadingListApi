c85589b7b11d2f6367875bd28bce0e3e00934dea
Mark Gravestock <mark.gravestock@gmail.com>
2015-11-14 11:31:35 +0000

    Improving coverage. Refactor duplication.

--- test/ReadingList.Persistance.UnitTests/RavenDbReadingListQueryTest.cs
+++ test/ReadingList.Persistance.UnitTests/RavenDbReadingListQueryTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Moq;
 using Ploeh.AutoFixture;
 using Raven.Client;
@@ -11,22 +12,45 @@ namespace ReadingList.Persistance.UnitTests
 {
     public class RavenDbReadingListQueryTest
     {
+        private Mock<IDocumentStore> documentStore;
+        private Mock<IDocumentSession> documentSession;
+        private RavenDbReadingListQuery sut;
+
+        public RavenDbReadingListQueryTest()
+        {
+            documentStore = new Mock<IDocumentStore>();
+            documentSession = new Mock<IDocumentSession>();
+            sut = new RavenDbReadingListQuery(documentStore.Object);
+        }
+
         [Fact]
         public void CanQueryForItemsToRead()
         {
-            var documentStore = new Mock<IDocumentStore>();
-            var documentSession = new Mock<IDocumentSession>();
-            var itemToRead = new Mock<IRavenQueryable<ItemToRead>>();
-            var sut = new RavenDbReadingListQuery(documentStore.Object);
             var expected = new Fixture().CreateMany<ItemToRead>().ToList();
 
+            var itemToReadQueryable = new Mock<IRavenQueryable<ItemToRead>>();
+
             documentStore.Setup(x => x.OpenSession()).Returns(documentSession.Object);
-            documentSession.Setup(x => x.Query<ItemToRead>()).Returns(itemToRead.Object);
-            itemToRead.Setup(x => x.GetEnumerator()).Returns(expected.GetEnumerator());
+            documentSession.Setup(x => x.Query<ItemToRead>()).Returns(itemToReadQueryable.Object);
+            itemToReadQueryable.Setup(x => x.GetEnumerator()).Returns(expected.GetEnumerator());
 
             var actual = sut.GetAllItemsToRead();
 
             actual.Should().Count.Exactly(expected.Count);
+        }
+
+        [Fact]
+        public void AnItemCanBeReadById()
+        {
+            var expectedId = new Fixture().Create<String>();
+            var expectedItemToRead = new Fixture().Create<ItemToRead>();
+
+            documentStore.Setup(x => x.OpenSession()).Returns(documentSession.Object);
+            documentSession.Setup(x => x.Load<ItemToRead>(expectedId)).Returns(expectedItemToRead);
+
+            var actual = sut.GetItemToRead(expectedId);
+
+            actual.Should().Equal(expectedItemToRead);
         }
     }
 }
